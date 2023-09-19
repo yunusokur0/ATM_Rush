ab46d3c1e7d20dad22473d782da8d12327e45dab
YUNUS <114630722+yunusokur0@users.noreply.github.com>
2023-09-19 11:42:12 +0300

    Update StackTypeUpdaterCommand.cs

--- Assets/Scripts/Runtime/Commands/Stack/StackTypeUpdaterCommand.cs
+++ Assets/Scripts/Runtime/Commands/Stack/StackTypeUpdaterCommand.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 
 namespace Assets.Scripts.Runtime.Commands.Stack
 {
-    //stack turu gunceler
     public class StackTypeUpdaterCommand
     {
         private List<GameObject> _collectableStack;
         private int _totalListScore;
 
-        //listeyi StackMangerden alir
         public StackTypeUpdaterCommand(ref List<GameObject> collectableStack)
         {
             _collectableStack = collectableStack;
         }
 
-        //listenin içerisinde bulunan öğelerin her birinin değerini toplayar
         public void Execute()
         {
             _totalListScore = 0;
