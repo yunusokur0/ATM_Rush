cea2820701e158ee87ee96dceb35071c8a79e8af
YUNUS <114630722+yunusokur0@users.noreply.github.com>
2023-09-19 11:38:40 +0300

    Update InputData.cs

--- Assets/Scripts/Runtime/Data/ValueObject/InputData.cs
+++ Assets/Scripts/Runtime/Data/ValueObject/InputData.cs
@@ -6,11 +6,8 @@ namespace Assets.Scripts.Runtime.Data.ValueObject
     [Serializable]
     public class InputData
     {
-        //yatay Giris Hizi, yatay olarak objenin gitme hizi
         public float HorizontalInputSpeed;
-        //border
         public float2 ClampSides;
-        //nesnenin hızını ne kadar hızlı yavaşlatması gerektiğini kontrol eder.
         public float ClampSpeed;
     }
 }
