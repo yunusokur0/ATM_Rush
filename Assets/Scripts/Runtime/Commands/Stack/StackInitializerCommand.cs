1b5d69ece48f22a5495cb5da746301f51a38ca93
YUNUS <114630722+yunusokur0@users.noreply.github.com>
2023-09-19 11:41:04 +0300

    Update StackInitializerCommand.cs

--- Assets/Scripts/Runtime/Commands/Stack/StackInitializerCommand.cs
+++ Assets/Scripts/Runtime/Commands/Stack/StackInitializerCommand.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 namespace Assets.Scripts.Runtime.Commands.Stack
 {
-    //button ile alinan pralari tutar
     public class StackInitializerCommand
     {
         private StackManager _stackManager;
         private GameObject _money;
-
-        //Parametreyi StackManagerden alir, money'de StackManagerden alir
         public StackInitializerCommand(StackManager stackManager,
             ref GameObject money)
         {
@@ -23,11 +20,11 @@ namespace Assets.Scripts.Runtime.Commands.Stack
             var stackLevel = CoreGameSignals.Instance.onGetStackLevel();
             for (int i = 1; i < stackLevel; i++)
             {
-                //money objesini Instanteder ve sonra ItemAdderOnStackCommand scriptine bak
+
                 GameObject obj = Object.Instantiate(_money);
                 _stackManager.AdderOnStackCommand.Execute(obj);
             }
-            //su kismi sonra anla
+
             _stackManager.StackTypeUpdaterCommand.Execute();
         }
     }
