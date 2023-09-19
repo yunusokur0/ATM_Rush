9c4e08294aa56d3f0eb2e13c364c9b1488da47cf
YUNUS <114630722+yunusokur0@users.noreply.github.com>
2023-09-19 11:41:40 +0300

    Update StackJumperCommand.cs

--- Assets/Scripts/Runtime/Commands/Stack/StackJumperCommand.cs
+++ Assets/Scripts/Runtime/Commands/Stack/StackJumperCommand.cs
@@ -20,7 +20,6 @@ namespace Assets.Scripts.Runtime.Commands.Stack
 
         public void Execute(int last, int index)
         {
-            // last ve index ayni rakam olursa for dongusu calismaz direk zaten setactive olur ItemRemoverOnStackCommand de
             for (int i = last; i > index; i--)
             {
 
@@ -28,18 +27,14 @@ namespace Assets.Scripts.Runtime.Commands.Stack
                 _collectableStack[i].transform.SetParent(_levelHolder.transform.GetChild(0));
                 _collectableStack[i].transform.DOJump(
                     new Vector3(
-                        //yatayda rastgele bir yönde zıplatır
                         Random.Range(-_data.JumpItemsClampX, _data.JumpItemsClampX + 1),
                         0.5f,
                         _collectableStack[i].transform.position.z + Random.Range(10, 15)),
-                    // nesnenin zıplama kuvvetini belirler
                     _data.JumpForce,
-                    //zıplama sırasındaki dönüş sayısını rastgele olarak belirler, Zıplama animasyonunun süresini belirler.
                     Random.Range(1, 3), 0.5f
                 );
                 _collectableStack[i].transform.DOScale(Vector3.one, 0);
                 _collectableStack.RemoveAt(i);
-                // listenin belegini temizler
                 _collectableStack.TrimExcess();
             }
         }
