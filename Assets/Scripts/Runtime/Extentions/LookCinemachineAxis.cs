18e3b6eb8e3d85ebd4e653d8a6491a3b6fa6ede4
YUNUS <114630722+yunusokur0@users.noreply.github.com>
2023-09-19 11:38:11 +0300

    Update LookCinemachineAxis.cs

--- Assets/Scripts/Runtime/Extentions/LookCinemachineAxis.cs
+++ Assets/Scripts/Runtime/Extentions/LookCinemachineAxis.cs
@@ -2,11 +2,8 @@
 using UnityEngine;
 namespace Assets.Scripts.Runtime.Extentions
 {
-    //bu komponentin Editör modunda da çalışmasına izin verir
     [ExecuteInEditMode]
-    //bu komponentin oyunu oynarken yapılan değişikliklerini kaydetmesine izin verir
-    [SaveDuringPlay]
-    //bu komponenti sürükle-bırak veya menüden eklemek yerine, kod içinde başka bir nesneye eklemek için kullanılır
+    [SaveDuringPlay
     [AddComponentMenu("")]
     public class LookCinemachineAxis : CinemachineExtension
     {
@@ -17,14 +14,10 @@ namespace Assets.Scripts.Runtime.Extentions
             CinemachineVirtualCameraBase vcam,
             CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
         {
-            //eğer işlenen aşama "Body" aşamasıysa, yani kameranın pozisyonunun hesaplandığı aşamadaysak, aşağıdaki kodları çalıştırır.
             if (stage == CinemachineCore.Stage.Body)
             {
-                //mevcut kamera pozisyonunu pos adlı bir değişkene kopyalar
                 var pos = state.RawPosition;
-                //kameranın X pozisyonunu m_XPosition değeri ile değiştirir
                 pos.x = m_XPosition;
-                //kameranın pozisyonunu günceller ve sabitlenmiş X pozisyonunu atar. Bu sayede, kameranın X pozisyonu istenen değere sabitlenir
                 state.RawPosition = pos;
             }
         }
